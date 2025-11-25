23dd5ec5fcf7c8f40cdd194deace9defc2f3f47d
lonblan <lonblan6@gmail.com>
2025-11-24 22:31:27 -0500

    Actualizamos

--- sección 4 estructuras decontrol/ejercicios/menu opciones.py
+++ sección 4 estructuras decontrol/ejercicios/menu opciones.py
@@ -1,11 +1,15 @@
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+# Menú de opciones
 
 print("Bienvenido al menú\n")
+
+# Bucle infinito para mostrar el menú hasta que el usuario decida terminar
 while True:
     print("Escriba una opcion:\n 1. Comparar dos números \n 2. Introducir un número impar \n 3. Terminar programa\n")
     opcion = input()
     if opcion == "1":
+# Opción 1: Comparar dos números
+        
         print("Introduzca un número:")
         a = int(input())
         print("Introduzca otro número")
@@ -15,7 +19,9 @@ while True:
         elif a > b:
             print(f"El número {a} es mayor que {b}")
         else:
-            print(f"El número {b} es mayor que {a}")
+            print(f"El número {b} es mayor que el número {a}")
+
+# Opción 2: Introducir un número impar
 
     elif opcion == "2":
         print("Introduzca un número impar:")
@@ -27,6 +33,8 @@ while True:
 
     elif opcion == "3":
         print("Programa terminado")
+
+# Salir del bucle y terminar el programa
         break
 
 
