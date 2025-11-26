73c66dddf3ddc9a6dc64a4c15e7b14a320ccf85f
lonblan <lonblan6@gmail.com>
2025-11-25 23:21:33 -0500

    Create bucle for.py

---
+++ sección 4 estructuras decontrol/ejercicios/bucle for.py
@@ -0,0 +1,3 @@
+libros = {"Cien años de soledad" : "1976", "Don Quijote de la Mancha" : "1954", "La sombra del viento" : "2001", "El amor en los tiempos del cólera" : "1985", "1984" : "1949"}
+for titulo in libros:
+    print(titulo)
